29f84456c7f9e40085d37cde8fa9005a3d76031c
Austin Ginn <austinleeginn@gmail.com>
2022-05-20 13:52:32 -0400

    created problems folder to show camera limitations

---
+++ problems/zoomSpeed.mjs
@@ -0,0 +1,18 @@
+//Zoom speed limitations of Alfatron 1080p PTZ cameras via ONVIF
+
+import OnvifManager from 'onvif-nvt';
+
+OnvifManager.connect('192.168.5.163', 2000, 'admin', 'admin')
+  .then(results => {
+    let camera = results
+
+    if (camera.ptz) { // PTZ is supported on this device
+      let vector = { x: .01, y: 0, z: .01 } 
+      camera.ptz.continuousMove(null, vector)
+        .then(() => {
+          setTimeout(() => {
+            camera.ptz.stop()
+          }, 10000) // stop the camera after 10 seconds
+        });
+    }
+  })
