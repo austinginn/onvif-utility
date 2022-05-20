02240dc2c88bbb17f436e3ad82186f4bc5a45f13
Austin Ginn <austinleeginn@gmail.com>
2022-05-19 23:00:58 -0400

    ready for testing

--- testing.mjs
+++ testing.mjs
@@ -4,42 +4,27 @@
 //TODO
 //disconnect and reconnect handling
 //verbose mode for debugging
-//simple ui for connection status
+//simple ui for connection status and keybinding
 //refactor camera connect function
-//key mapping because hardcoding is bad
-//move keyboard input handling into a function
 import EventEmitter from 'events';
 import OnvifManager from 'onvif-nvt';
+import fs from 'fs';
 
+//load config files
+let rawdata = fs.readFileSync('cameras.json');
+let cameras = JSON.parse(rawdata);
+console.log("cameras.json loaded");
+
+rawdata = fs.readFileSync('keybinds.json');
+let keybinds = JSON.parse(rawdata);
+console.log("keybinds.json loaded");
+
+//Global Constants
 const eventEmitter = new EventEmitter();
-
-//array of cameras
-const cameras = [{
-    id: 1,
-    name: "Cam1",
-    ip: "192.168.1.51",
-    port: 2000,
-    user: "admin",
-    pass: "admin"
-}, {
-    id: 2,
-    name: "Cam2",
-    ip: "192.168.1.52",
-    port: 2000,
-    user: "admin",
-    pass: "admin"
-}, {
-    id: 3,
-    name: "Cam3",
-    ip: "192.168.1.53",
-    port: 2000,
-    user: "admin",
-    pass: "admin"
-}]
-
-const VELOCITY = .01; // multiply by -1 depending on direction
+const VELOCITY = .01; // multiply by -1 depending on direction --- this is minimum value supported
 const MAX_CONNECTION_ATTEMPTS = 5;
 
+//Globals
 let selected = 1; //current selected camera id -- default to 1
 
 //Main because async...
@@ -49,6 +34,7 @@ const main = async () => {
     //////////////////////////
     eventEmitter.on("message", (msg) => {
         console.log(msg);
+        //push to ui
     });
 
     ////////////////
@@ -62,59 +48,114 @@ const main = async () => {
     ///////////////////////////
     //KEYBOARD INPUT HANDLING//
     ///////////////////////////
-    //Handle keyboard input
+    control();
+}
+
+//Connect to a camera and handle events
+//currently no reconnect trys on a disconnect
+//onvif dependency connection timeout is unknown
+//test disconnect cases and then handle them in a catch block for reconnect functionality
+const connect = async (ip = "192.168.1.1", port = 2000, user = "admin", pass = "admin", id = "1", attempt = 0) => {
+    //connect
+    try {
+        let onvif = new OnvifManager; //documentation not clear if this actually handles multiple connections of if you need a seperate object for each connection
+        let cam = await onvif.connect(ip, port, user, pass);
+        console.log(cam);
+    } catch (err) { //catch block for initial onvif connection failure
+        attempt++;
+        console.log(ip + ":" + port + " | Connection attempt #" + attempt);
+
+        if (attempt < MAX_CONNECTION_ATTEMPTS) { connect(ip, port, user, pass, id, attempt); }
+        else { console.log(ip + ":" + port + " | Connection failed"); }
+        return -1;
+    }
+
+    eventEmitter.emit("message", "Camera " + id + ": Connected!");
+
+    if (!cam.ptz) { throw err } //ptz controls not supported by camera -- reconnect attempt?
+
+    eventEmitter.on("camera_" + id, async (cmd) => {
+        let vector = { x: 0, y: 0, z: 0 } //no movement
+        try {
+            switch (cmd) {
+                case "stop":
+                    await cam.ptz.stop();
+                    break;
+                case "left":
+                    vector = { x: VELOCITY, y: 0, z: 0 }; //left
+                    await cam.ptz.continuousMove(null, vector);
+                    break;
+                case "right":
+                    vector = { x: VELOCITY * -1, y: 0, z: 0 }; //right
+                    await cam.ptz.continuousMove(null, vector);
+                    break;
+                case "up":
+                    vector = { x: 0, y: VELOCITY, z: 0 }; //up
+                    await cam.ptz.continuousMove(null, vector);
+                    break;
+                case "down":
+                    vector = { x: 0, y: VELOCITY * -1, z: 0 }; //up
+                    await cam.ptz.continuousMove(null, vector);
+                    break;
+                default:
+                    break;
+            }
+        } catch (err) { console.log(err); }
+    });
+}
+
+//keyboard input handling
+const control = () => {
     let stdin = process.stdin;
 
-    //Enter = bad
+    //enter = bad
     stdin.setRawMode(true);
 
     //resume stdin in the parent process (node app won't quit all by itself unless an error or process.exit() happens)
     stdin.resume();
 
-    //Screw binanry
+    //screw binanry
     stdin.setEncoding('utf8');
 
-    //On any data into stdin
+    //on any data into stdin
     stdin.on('data', function (key) {
-        // ctrl-c ( end of text )
-        if (key === '\u0003') {
-            process.exit();
-        }
+        // ctrl-c
+        if (key === '\u0003') { process.exit(); }
 
         switch (key) {
-            case '1': //select 1
+            case keybinds.cam1: //select 1
                 console.log("Camera 1 Selected");
                 selected = 1;
                 break;
-            case '2': //select 2
+            case keybinds.cam2: //select 2
                 console.log("Camera 2 Selected");
                 selected = 2;
                 break;
-            case '3': //select 3
+            case keybinds.cam3: //select 3
                 console.log("Camera 3 Selected");
                 selected = 3;
                 break;
-            case 'a': //pan left
+            case keybinds.panLeft: //pan left
                 console.log("Commanding Cam " + selected + " to pan left");
                 ptz(selected, "left");
                 break;
-            case 'd': //pan right
+            case keybinds.panRight: //pan right
                 console.log("Commanding Cam " + selected + " to pan right");
                 ptz(selected, "right");
                 break;
-            case 'w': //tilt up
+            case keybinds.tiltUp: //tilt up
                 console.log("Commanding Cam " + selected + " to tilt up");
                 ptz(selected, "up");
                 break;
-            case 's': //tilt down
+            case keybinds.tiltDown: //tilt down
                 console.log("Commanding Cam " + selected + " to tilt down");
                 ptz(selected, "up");
                 break;
-            case 'z': //STOP ALL
+            case keybinds.stopAll: //STOP ALL
                 console.log("Commanding all cams to stop");
                 hault();
                 break;
-            case 'x': //STOP ALL
+            case keybinds.stop: //STOP 
                 console.log("Commanding Cam " + selected + " to stop");
                 hault(selected);
                 break;
@@ -122,68 +163,6 @@ const main = async () => {
                 console.log("key not assigned");
         }
     });
-}
-
-//Connect to a camera and handle events
-//currently no reconnect trys on a disconnect
-//onvif dependency connection timeout is unknown (but seems to indefinitely attempt connection)
-//test disconnect cases and then handle them in a catch block for reconnect functionality
-const connect = async (ip, port = 2000, user = "admin", pass = "admin", id, attempt = 0) => {
-    //connect
-    try{
-        let onvif = new OnvifManager; //documentation not clear if this actually handles multiple connection of if you need a seperate object for each connection
-        let cam = await onvif.connect(ip, port, user, pass);
-        console.log(cam);
-    } catch(err){ //catch block for initial onvif connection failure
-        // console.log(err); 
-        attempt++;
-        console.log(ip + ":" + port +  " | Connection attempt #" + attempt);
-        
-        if(attempt < MAX_CONNECTION_ATTEMPTS){
-            connect(ip, port, user, pass, id, attempt);
-        
-        } else {
-            console.log(ip + ":" + port +  " | Connection failed");
-        }
-        return -1;
-    }
-    
-    eventEmitter.emit("message", "Camera " + id + ": Connected!");
-
-    //set up camera event handling here
-    //
-
-    if (cam.ptz) { //PTZ is supported
-        //PTZ event handling
-        let direction = { x: 0, y: 0, z: 0 } //no movement
-        eventEmitter.on("camera_" + id, async (cmd) => {
-            try {
-                switch (cmd) {
-                    case "stop":
-                        await cam.ptz.stop();
-                        break;
-                    case "left":
-                        direction = { x: VELOCITY, y: 0, z: 0 }; //left
-                        await cam.ptz.continuousMove(null, direction);
-                        break;
-                    case "right":
-                        direction = { x: VELOCITY * -1, y: 0, z: 0 }; //right
-                        await cam.ptz.continuousMove(null, direction);
-                        break;
-                    case "up":
-                        direction = { x: 0, y: VELOCITY, z: 0 }; //up
-                        await cam.ptz.continuousMove(null, direction);
-                        break;
-                    case "down":
-                        direction = { x: 0, y: VELOCITY * -1, z: 0 }; //up
-                        await cam.ptz.continuousMove(null, direction);
-                        break;
-                    default:
-                        break;
-                }
-            } catch (err) { console.log(err); }
-        });
-    }
 }
 
 //Fire ptz event
